d2eb160abf7fd2af608765726784701337b2977e
Luis David <david12.29@hotmail.com>
2024-05-29 13:49:16 -0500

    Add feature: Person class has id,firstName, lastName and email.

--- src/main/java/com/david/apirestSpringBoot/Person/Person.java
+++ src/main/java/com/david/apirestSpringBoot/Person/Person.java
@@ -1,8 +1,17 @@
 package com.david.apirestSpringBoot.Person;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
 @Entity
 public class Person {
-    
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
 }
