43cfe8462f77982a677314411be09a8f1368ded0
Luis David <david12.29@hotmail.com>
2024-05-29 13:54:49 -0500

    Update, Person class has some change about annotations.

--- src/main/java/com/david/apirestSpringBoot/Person/Person.java
+++ src/main/java/com/david/apirestSpringBoot/Person/Person.java
@@ -1,14 +1,25 @@
 package com.david.apirestSpringBoot.Person;
 
+import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 @Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+
+    @Basic
     private String firstName;
     private String lastName;
     private String email;
