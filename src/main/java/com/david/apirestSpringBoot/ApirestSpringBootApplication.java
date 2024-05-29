ef8d8a4312b3befb15c742545dfe47d596067055
Luis David <david12.29@hotmail.com>
2024-05-29 13:33:30 -0500

    Create, I Created this project.

---
+++ src/main/java/com/david/apirestSpringBoot/ApirestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.david.apirestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/apirestSpringBoot/ApirestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.apirestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
