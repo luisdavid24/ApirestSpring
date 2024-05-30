a3a4034aef2cbd169f5bdd6880ec0eb4436adab7
Luis David <david12.29@hotmail.com>
2024-05-30 11:15:27 -0500

    Refactor, PersonService class can create Person

--- src/main/java/com/david/apirestSpringBoot/Person/PersonService.java
+++ src/main/java/com/david/apirestSpringBoot/Person/PersonService.java
@@ -1,5 +1,18 @@
 package com.david.apirestSpringBoot.Person;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class PersonService {
+
+    private final PersonRepository personRepo;
+
+
+    public void createPerson(Person person){
+            personRepo.save(person);
+    }
 
 }
