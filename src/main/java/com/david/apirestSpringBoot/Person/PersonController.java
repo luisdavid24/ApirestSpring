bbc6077bb02455fbdf13ace3d177df28ce8f8028
Luis David <david12.29@hotmail.com>
2024-05-30 11:42:46 -0500

    Refactor, Person controller has a new method about create person

--- src/main/java/com/david/apirestSpringBoot/Person/PersonController.java
+++ src/main/java/com/david/apirestSpringBoot/Person/PersonController.java
@@ -1,5 +1,22 @@
 package com.david.apirestSpringBoot.Person;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/person")
+@RequiredArgsConstructor
 public class PersonController {
 
+    private final PersonService personService;
+
+    @PostMapping
+    public void createPerson(@RequestBody Person person){
+        personService.createPerson(person);
+    }
 }
