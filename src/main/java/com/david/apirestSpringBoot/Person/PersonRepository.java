a01c4a2f3d9bab00402c5fbd870d5a66861c8424
Luis David <david12.29@hotmail.com>
2024-05-30 11:04:07 -0500

    Refactor, PersonRepository  extends  JpaRepository

--- src/main/java/com/david/apirestSpringBoot/Person/PersonRepository.java
+++ src/main/java/com/david/apirestSpringBoot/Person/PersonRepository.java
@@ -1,5 +1,10 @@
 package com.david.apirestSpringBoot.Person;
 
-public interface  PersonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface  PersonRepository  extends  JpaRepository<Person, Integer>{
+    
 
 }
